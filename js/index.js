c97c2ebbf73a180244f43656b70fba3a48c18e74
JadYaghi <59618437+JadYaghi@users.noreply.github.com>
2020-09-18 14:37:42 -0600

    cool

--- js/index.js
+++ js/index.js
@@ -1,4 +1,22 @@
-console.log('hp')
+const calculateButton = document.querySelector('label')
+const shippingWeight = document.querySelector('input')
+const shippingRateDisplay = document.querySelector('p')
+const shippingRate = 10;
 
-console.log(document.getElementById('weight'))
+calculateButton.addEventListener('click',function(){
+    // reading in = user input
+    // data type of text field = string
+    // string number boolean t/f
+    // console.log(typeof(weight)) for type
+    // string +
+    // Numbers * + - ? arathmetics operators
+    let weight = shippingWeight.value
+    // console.log(weight*10)
+    weight = Number(weight)
+    let rate = weight * shippingRate;
+    shippingRateDisplay.textContent = '$' + rate + '.00'
+    shippingRateDisplay.style.fontSize = "3rem"
+})
 
+// NaN is a number, also hiccup of the language
+// Typescript
